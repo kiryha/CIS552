289d6c2a3227df2c56033b082bc71424e8407dc5
Kiryha <kiryha@gmail.com>
2024-02-08 18:18:45 -0500

    text to speech

--- assignments/lab_4/text_to_speech.py
+++ assignments/lab_4/text_to_speech.py
@@ -1,6 +1,6 @@
 from gtts import gTTS
 
-speech = """This course introduces basic techniques for visualization, quantitative analysis, intelligent visual 
+speech_en = """This course introduces basic techniques for visualization, quantitative analysis, intelligent visual 
 understanding, virtualization, digital animation, computer and video games, 
 and web multimedia. Topics include data visualization, computer vision, visual analysis, 
 the process of creating animated video clips from start to finish (including story 
@@ -10,5 +10,14 @@ multimedia, real-time rendering, visualization tools, and virtual machines.
 """
 
 
-tts = gTTS(speech)
-tts.save('waw/speech_en.mp3')
+tts_en = gTTS(speech_en, lang='en')
+tts_en.save('waw/speech_en.mp3')
+
+tts_fr = gTTS(speech_en, lang='fr')
+tts_fr.save('waw/speech_fr.mp3')
+
+tts_fr = gTTS(speech_en, lang='es')
+tts_fr.save('waw/speech_es.mp3')
+
+tts_fr = gTTS(speech_en, lang='ar')
+tts_fr.save('waw/speech_ar.mp3')
